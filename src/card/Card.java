1fc2758d290e64d32250560be9edbbe247c0a97f
migue <migue@LAPTOP-IRVQF72N>
2019-01-25 14:22:09 -0500

    created .igitignore

---
+++ src/card/Card.java
@@ -0,0 +1,55 @@
+   /*
+        [Class name]
+        Author: Jose Miguelle Dela Paz
+        Date: [date]
+
+        Description
+        [Description about the class]
+    */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package card;
+
+/**
+ *
+ * @author migue
+ */
+public class Card {
+    private String suit;
+    private int value;
+    
+    
+    
+    public Card(String givenSuit, int givenValue){
+        suit = givenSuit;
+        value = givenValue;       
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" + "suit=" + suit + ", value=" + value + '}';
+    }
+    
+    
+    
+}
